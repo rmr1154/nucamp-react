80c5adf32d7891cc202534e3f10585eb7fd4c654
rmr1154 <38836662+rmr1154@users.noreply.github.com>
2020-05-02 08:50:07 -0400

    Adding to the Single Page App

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,24 +1,66 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
-    render () {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        };
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+    render() {
         return (
             <React.Fragment>
                 <Jumbotron fluid>
                     <div className="container">
-                        <div className="col">
-                            <h1>NuCamp
+                        <div className="row">
+                            <div className="col">
+                                <h1>NuCamp</h1>
                                 <h2>a better way to camp</h2>
-                            </h1>
+                            </div>
                         </div>
                     </div>
                 </Jumbotron>
-                <navbar dark sticky="top">
+                <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <navbarBrand href="/">NuCamp</navbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <i className="fa fa-home fa-lg" /> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/directory">
+                                        <i className="fa fa-list fa-lg" /> Directory
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <i className="fa fa-info fa-lg" /> About
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <i className="fa fa-address-card fa-lg" /> Contact Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
-                </navbar>
+                </Navbar>
             </React.Fragment>
         );
     }
