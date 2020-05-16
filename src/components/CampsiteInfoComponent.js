1daacdc530bba79d64f14c82a33eedafd69fabf1
rmr <rmr@micro-logix.com>
2020-05-16 10:23:54 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -32,6 +32,7 @@ class CommentForm extends Component {
         console.log('Current state is: ' + JSON.stringify(values));
         alert('Current state is: ' + JSON.stringify(values));
         this.toggleModal();
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -104,7 +105,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <>
@@ -123,7 +124,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                     })
 
                     }
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             </>);
     }
@@ -169,7 +170,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}      
                         campsiteId={props.campsite.id}
                     />
                 </div>
